68041037805bb932a340f56dc55c5847bcba3a56
Nathan Barry <nathanb@symbiote.com.au>
2021-07-05 16:59:24 +1000

    feat(Add Documentation)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,9 @@
 const path = require("path");
 
 module.exports = {
-  stories: ["../stories/**/*.stories.tsx"],
+  stories: ["../stories/**/*.stories.@(tsx|mdx)"],
   // Add any Storybook addons you want here: https://storybook.js.org/addons/
-  addons: [],
+  addons: ['@storybook/addon-docs'],
   webpackFinal: async (config) => {
     config.module.rules.push({
       test: /\.scss$/,
---
+++ .storybook/preview.js
@@ -0,0 +1,8 @@
+
+export const parameters = {
+    options: {
+      storySort: {
+        order: ['Design System', ['Introduction', 'Philosophy', 'Getting Started'], 'Components', ['Atoms', 'Molecules', 'Organisms']],
+      },
+    },
+  };
