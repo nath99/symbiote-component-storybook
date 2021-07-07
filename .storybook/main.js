a05044cd59a015f75d80a2c97566e1d8abf99d11
Amir Sarkis <amir@symbiote.com.au>
2021-07-07 15:41:31 +1000

    Added Card component story

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,13 +2,24 @@ const path = require("path");
 
 module.exports = {
   stories: ["../stories/**/*.stories.@(tsx|mdx)"],
+
   // Add any Storybook addons you want here: https://storybook.js.org/addons/
   addons: ['@storybook/addon-docs', '@storybook/addon-essentials'],
+
+  scripts: {
+    "start-storybook": "start-storybook -s ../public -p 9001"
+  },
+
   webpackFinal: async (config) => {
     config.module.rules.push({
       test: /\.scss$/,
       use: ["style-loader", "css-loader", "sass-loader"],
       include: path.resolve(__dirname, "../")
+    });
+
+    config.module.rules.push({
+      test: /\.(svg|png|jpg|jpeg|gif)$/i,
+      loader: 'file-loader'
     });
 
     config.module.rules.push({
@@ -18,6 +29,7 @@ module.exports = {
         presets: [["react-app", { flow: false, typescript: true }]]
       }
     });
+
     config.resolve.extensions.push(".ts", ".tsx");
 
     return config;
