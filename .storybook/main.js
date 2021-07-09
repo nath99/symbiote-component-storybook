2014bf97a0a3822b875c5da1859210d0eb039c0b
Nathan Barry <nathanb@symbiote.com.au>
2021-07-09 11:15:57 +1000

    Add grid and colour demo css

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,7 +4,7 @@ module.exports = {
   stories: ["../stories/**/*.stories.@(tsx|mdx)"],
 
   // Add any Storybook addons you want here: https://storybook.js.org/addons/
-  addons: ['@storybook/addon-essentials', '@storybook/addon-docs', '@storybook/addon-a11y'],
+  addons: ['@storybook/addon-essentials', '@storybook/preset-scss', '@storybook/addon-docs', '@storybook/addon-a11y'],
 
   scripts: {
     "start-storybook": "start-storybook -s ../public -p 9001"
