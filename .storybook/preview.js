15995b506933b01e2f55264aff971affbb06da49
Amir Sarkis <amir@symbiote.com.au>
2021-08-06 14:09:53 +1000

    Added documentation for Carousel component

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -9,7 +9,7 @@ export const parameters = {
             'Navigation', ['Main Navigation', 'Side Navigation', 'Breadcrumbs', 'Footer'],
             'Interactions', ['Buttons', 'Toggle', 'Checkbox', 'Inputs', 'Radio', 'Select', 'Slider'],
             'Alerts', ['Notification', 'Page alert'],
-            'Content', ['Hero', 'Table', 'List', 'Accordion', 'Cards', 'Tooltip', 'Label'],
+            'Content', ['Hero', 'Table', 'List', 'Accordion', 'Cards', 'Tooltip', 'Label', 'Carousel'],
             'Page Elements', ['Progress bar', 'Pagination']
           ],
         ],
