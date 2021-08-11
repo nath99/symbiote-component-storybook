98d480055cc6e87c1e584706e624321fef27e369
Amir Sarkis <amir@symbiote.com.au>
2021-08-11 10:47:46 +1000

    Added documentation for DatePicker component

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -7,7 +7,7 @@ export const parameters = {
             'Foundations', ['Color', 'Typography', 'Icons'],
             'Layout', ['Grid', 'Spacing'],
             'Navigation', ['Main Navigation', 'Side Navigation', 'Breadcrumbs', 'Footer'],
-            'Interactions', ['Buttons', 'Toggle', 'Checkbox', 'Inputs', 'Radio', 'Select', 'Slider'],
+            'Interactions', ['Buttons', 'Toggle', 'Checkbox', 'Inputs', 'Radio', 'Select', 'Slider', 'DatePicker'],
             'Alerts', ['Notification', 'Page alert'],
             'Content', ['Hero', 'Table', 'List', 'Accordion', 'Cards', 'Tooltip', 'Label', 'Carousel', 'Modal'],
             'Page Elements', ['Progress bar', 'Pagination']
