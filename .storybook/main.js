7145d153cc6e0b49028da2de456b3f447d64e89f
Amir Sarkis <amir@symbiote.com.au>
2021-08-19 14:19:16 +1000

    Added change to webpack which fixes broken paths for static builds in SS

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,29 @@
 const path = require("path");
+const webpack = require('webpack');
+const HookShellScriptPlugin = require('hook-shell-script-webpack-plugin');
+
+/**
+ * Disables Webpack from splitting the code into chunks
+ * @param config - The webpack config to update
+ */
+function disableChunkSplitting(config) {
+  config.optimization = { splitChunks: { chunks: 'async' } };
+  config.output = { ...config.output, chunkFilename: '[chunkhash].chunk.js', };
+  config.plugins.push(new webpack.optimize.LimitChunkCountPlugin({ maxChunks: 1, }));
+
+  return config;
+}
 
 module.exports = {
   stories: ["../stories/**/*.stories.@(tsx|mdx)"],
 
   // Add any Storybook addons you want here: https://storybook.js.org/addons/
   addons: ['@storybook/addon-docs', '@storybook/addon-essentials', '@storybook/preset-scss', '@storybook/addon-a11y'],
+
+  managerWebpack: async (config) => {
+    return disableChunkSplitting(config);
+  },
+
   webpackFinal: async (config) => {
     config.module.rules.push({
       test: /\.scss$/,
@@ -25,8 +44,13 @@ module.exports = {
       }
     });
 
+    // Post emit plugin which fixes paths to static media files and iframe.htm when doing build
+    config.plugins.push(new HookShellScriptPlugin({
+      afterEmit: ['sed -i -e "s#static/media/#.storybook/static/media/#g;s#iframe.html#.storybook/iframe.html#g" storybook-static/main.4f3f9c96450c4fd9e74b.manager.bundle.js']
+    }))
+
     config.resolve.extensions.push(".ts", ".tsx");
 
-    return config;
-  }
+    return disableChunkSplitting(config);
+  },
 };
